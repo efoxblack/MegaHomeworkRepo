7b6fc4e697afa35218685b05def265d63a926380
ErikFox <erikmichaelfox@gmail.com>
2020-03-25 14:02:43 -0400

    Add employer and calendar tests/notes

---
+++ src/main/java/employer/Employee.java
@@ -0,0 +1,46 @@
+package employer;
+
+import java.util.Random;
+
+public class Employee {
+    private static int nextId;
+
+    private int id;
+    private String name = "";
+    private double salary;
+
+    static {
+        Random generator = new Random();
+        nextId = generator.nextInt(10000);
+    }
+
+    {
+        id = nextId;
+        nextId++;
+    }
+
+    public Employee(String n, double s) {
+        name = n;
+        salary = s;
+    }
+
+    public Employee(double s) {
+        this("Employee #" + nextId, s);
+    }
+
+    public Employee() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
